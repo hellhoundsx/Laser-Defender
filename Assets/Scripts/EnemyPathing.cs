9c455611c3e4298ad365d7b5188874eaf79f67b4
Ricardo Gomes <ricardogomes.1995@outlook.pt>
2021-04-22 20:51:15 +0200

    add waypoints

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
